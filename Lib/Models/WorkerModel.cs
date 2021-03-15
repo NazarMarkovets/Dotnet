938b9b7a80e255169540740943b9a4b780ba9c6d
NazarMarkovets <nazaran237@gmail.com>
2021-03-15 22:46:26 +0200

    feat: add worker class

---
+++ Lib/Models/WorkerModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Lib.Interfaces;
+
+namespace Lib.Models
+{
+    public class WorkerModel: IPerson
+    {
+        public string _personName { get; set; }
+        public int _personAge { get; set; }
+        public StatusEnum status { get; set; }
+
+        public WorkerModel(string name, int age)
+        {
+            _personAge = age;
+            _personName = name;
+            status = StatusEnum.Worker;
+        }
+        
+        private int WorkDay { get; set; }
+
+        public virtual void MakeWork()
+        {
+            Console.WriteLine($"I am a {status} and do my work for {WorkDay}");
+        }
+
+        
+    }
+}
